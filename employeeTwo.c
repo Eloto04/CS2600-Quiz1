d57b848088382ed4a421fcac69fd1a80b14eefe8
Eloto04 <eloto8677@gmail.com>
2023-10-27 23:50:41 -0700

    Updated employeeTwo

--- employeeTwo.c
+++ employeeTwo.c
@@ -12,11 +12,7 @@
 //()(The second set of parentheses is the arguments of the function call) 
 
 static PtrToEmployee searchEmployeeTable(PtrToConstEmployee ptr, int tableSize, const void *targetPtr,  
-
-                                        int (*functionPtr)(const void *, PtrToConstEmployee)) 
-
-{ 
-
+int (*functionPtr)(const void *, PtrToConstEmployee)) { 
     PtrToConstEmployee endPtr = ptr + tableSize; 
     for(; ptr < endPtr; ptr++) 
         if ((*functionPtr)(targetPtr, ptr) == 0) 
@@ -28,23 +24,21 @@ static PtrToEmployee searchEmployeeTable(PtrToConstEmployee ptr, int tableSize, 
 } 
 
 //The functionPtr will point to one of these comparison functions to perform a check 
-
-static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
-
-{ 
-
+static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr) { 
     return * (long *) targetPtr != tableValuePtr->number; //const void *targetPtr ==> typecast as int pointer then dereference 
-
 } 
 
-static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
-
-{ 
-
-    return strcmp((char *) targetPtr, tableValuePtr->name);//const void *targetPtr ==> typecast as char pointer then pass into strcmp() 
-
+static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr){ 
+    return strcmp((char *) targetPtr, tableValuePtr->name);//const void *targetPtr ==> typecast as char pointer then pass into strcmp()
 } 
 
+static int compareEmployeePhoneNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr){ 
+    return strcmp((char *) targetPtr, tableValuePtr->phone);//const void *targetPtr ==> typecast as char pointer then pass into strcmp()
+} 
+
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr) { 
+    return * (long *) targetPtr != tableValuePtr->salary; //const void *targetPtr ==> typecast as int pointer then dereference 
+} 
  
 
  
